a323c584eab7b649091108de5e1679a9e77dc0c4
piotr <PiotrBosak@yahoo.com>
2020-04-08 16:19:58 +0200

    26, updated MyList to use functions instead of traits for myPredicate and myTransformer

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -1,16 +1,7 @@
 package exercises
 
-import scala.collection.immutable.Stream.Cons
 
 
-trait MyPredicate[-T] {
-  def test(elem: T): Boolean
-}
-
-
-trait MyTransformer[-A, B] {
-  def transform(elem: A): B
-}
 
 
 
@@ -26,9 +17,9 @@ abstract sealed class MyList[+A] {
   override def toString: String = "[" + printElements() + "]"
 
   def printElements(): String
-  def filter(predicate: MyPredicate[A]): MyList[A]
-  def map[B](transformer: MyTransformer[A,B]) : MyList[B]
-  def flatMap[B](transformer: MyTransformer[A,MyList[B]]) : MyList[B]
+  def filter(predicate: A => Boolean): MyList[A]
+  def map[B](transformer: A => B) : MyList[B]
+  def flatMap[B](transformer:  A => MyList[B]) : MyList[B]
   def ++[B >: A](list: MyList[B]): MyList[B]
 
 }
@@ -45,11 +36,11 @@ case object EmptyList extends MyList[Nothing] {
 
   override def add[B >: Nothing](element: B): MyList[B] = new NonEmptyList[B](element, EmptyList)
 
-  override def map[B](transformer: MyTransformer[Nothing,B]): MyList[B] =EmptyList
+  override def map[B](transformer: Nothing =>B): MyList[B] =EmptyList
 
-  override def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = EmptyList
+  override def filter(predicate: Nothing => Boolean ): MyList[Nothing] = EmptyList
 
-  override def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = EmptyList
+  override def flatMap[B](transformer:  Nothing => MyList[B]): MyList[B] = EmptyList
    override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 
 }
@@ -70,48 +61,43 @@ case class NonEmptyList[A]( h: A, t: MyList[A]) extends MyList[A] {
 
   override def add[B >: A](n: B): MyList[B] = new NonEmptyList(n, this)
 
-  override def filter(predicate: MyPredicate[A]): MyList[A] = {
-    if(predicate.test(h)) new NonEmptyList(h,tail.filter(predicate))
+  override def filter(predicate: A => Boolean )= {
+    if(predicate(h)) new NonEmptyList(h,tail.filter(predicate))
     else tail.filter(predicate)
     }
 
 
 
-  override def map[B](transformer: MyTransformer[A, B]): MyList[B] = {
-    new NonEmptyList[B](transformer.transform(h),tail.map(transformer))
+  override def map[B](transformer: A => B): MyList[B] = {
+    new NonEmptyList[B](transformer(h),tail.map(transformer))
   }
 
-  override def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
-    transformer.transform(h) ++ t.flatMap(transformer)
+  override def flatMap[B](transformer: A => MyList[B]): MyList[B] =
+    transformer(h) ++ t.flatMap(transformer)
   }
 
 
 
 
 object Run extends App {
-  val list = new NonEmptyList(1, new NonEmptyList(7,EmptyList))
-  val list2 = new NonEmptyList(5,new NonEmptyList(3,EmptyList))
-  val evenPredicate = new EvenPredicate
-  val evenList = list2.filter(evenPredicate)
-  print(evenList.toString)
+
+
+  val list = NonEmptyList(1, NonEmptyList(7,EmptyList))
+  val list2 = NonEmptyList(5, NonEmptyList(4,EmptyList))
+  val evenList = list2.filter(e => e%2 == 0)
+  println(evenList.toString)
   val newList = list ++ list2
-  val flatList = newList.flatMap(new IncTransformer)
-  print(flatList.toString)
+  val flatList = newList.flatMap(e => NonEmptyList(e,NonEmptyList(e+1,EmptyList)))
+  println(flatList)
 
 
-  class EvenPredicate extends MyPredicate[Int]{
-    override def test(elem: Int): Boolean = elem%2 == 0
-  }
-
-  class IncTransformer extends MyTransformer[Int,MyList[Int]]{
-    override def transform(elem: Int): MyList[Int] = new NonEmptyList[Int](elem,new NonEmptyList(elem+1,EmptyList))
   }
 
 
 
 
 
-}
+
 
 
 
---
+++ src/functionalProgramming/WhatsAFunction.scala
@@ -0,0 +1,22 @@
+package functionalProgramming
+
+object WhatsAFunction extends App {
+
+  val stringToIntConverter: String => Int = e => e.toInt
+
+  stringToIntConverter("3")
+
+
+  //takes to string and concat
+  //MyList and transform myPredicate and transfomer into function types
+  //def function which takes an int and return a function whi`ch takes int and returns int
+
+  val concat: (String, String) => String = (e,f) => e.concat(f)
+
+  val fancyFunction : Int => (Int => Int) = (e => (x => x*e))
+  val first = fancyFunction(3)(5)// curried function
+  println(first)
+
+}
+
+
