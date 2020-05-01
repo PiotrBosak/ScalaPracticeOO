6d0ec5b463c86ab5f71fdf8ab5578c4356bf7566
piotr <PiotrBosak@yahoo.com>
2020-05-01 10:12:24 +0200

    finished exercise

--- src/PatternMatching/PatternMatchingBasics.scala
+++ src/PatternMatching/PatternMatchingBasics.scala
@@ -3,76 +3,57 @@ package PatternMatching
 import scala.util.Random
 
 object PatternMatchingBasics extends App {
-  val random = new Random
-  val x = random.nextInt(10)
-  val desc = x match {
-    case 1 => "one"
-    case 2 => "two"
-    case _ => "something else"
-  }
-  println(desc)
-
-  //1. can decompose valeus
-  //used with case classes
-
-  case class Person(name: String, age: Int)
-
-  val bob = Person("Bob", 20)
-
-  val greeting = bob match {
-    case Person(a, b) => s"I'm $a and I'm $b"
-    case _ => "I dont know who I am"
-  }
-  println(greeting)
-  val greeting2 = bob match {
-    case Person(a, b) if b > 21 => s"I'm $a and I'm $b"
-    case _ => "I dont know who I am"
-  }
-
-  println(greeting2)
-
-  //1. cases are matched in order
-  //type of pattern match is unification of all cases
-  //Pattern matching on sealed hierarchies
-  // works well with case classes
 
 
-  sealed class Animal
-
-  case class Dog(breed: String) extends Animal
-
-  case class Parrot(greeting: String) extends Animal
-
-  val animal: Animal = Dog("a")
-  animal match {
-    case Dog(someBreed) => println(s"matched a dog $someBreed")
-
-  }
   /*
   patternMatching uses PM
   takes
 
   sum(number(1), number(2)) => 1+2
    */
-  trait Expr
-  case class Number(n: Int) extends Expr
-  case class Sum(e1: Expr,e2:Expr)extends Expr
-  case class Prod(e1:Expr, e2:Expr) extends Expr
+  sealed trait Expr
 
-  def printExpression(expr: Expr):Unit = {
+  case class Number(n: Int) extends Expr
+
+  case class Sum(e1: Expr, e2: Expr) extends Expr
+
+  case class Prod(e1: Expr, e2: Expr) extends Expr
+
+  def printExpression(expr: Expr): Unit = {
     expr match {
-      case Number(something) => println(something)
-      case Sum(e1,e2) =>
+      case Number(something) => print(something)
+      case Sum(e1, e2) =>
+        print("(")
         printExpression(e1)
         print("+")
         printExpression(e2)
-      case Prod(e1,e2) =>
+        print(")")
+      case Prod(e1, e2) =>
         printExpression(e1)
         print("*")
         printExpression(e2)
     }
   }
 
-  printExpression(Sum(Number(1),Number(2)))
+  def betterShow(expr: Expr): String = {
+    expr match {
+      case Number(n) => n.toString
+      case Sum(e, f) => betterShow(e) + "+" + betterShow(f)
+      case Prod(e, f) =>
+        def maybeParenthesis(exp: Expr): String = {
+          exp match {
+            case Number(n) => n.toString
+            case Prod(g, h) => betterShow(g) + "*" + betterShow(h)
+            case Sum(g, h) =>
+              "(" + betterShow(g) + "+" + betterShow(h) + ")"
+          }
+        }
+
+        maybeParenthesis(e) + "*" + maybeParenthesis(f)
+
+    }
+  }
+
+  println(betterShow(Prod(Sum(Number(1), Number(2)), Sum(Number(1), Number(2)))))
 
 }
